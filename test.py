7bf1554567071391edca4218ef78b49f3c88fc99
Sandeep Raj Ganji <deepuganji@gmail.com>
2025-06-16 16:25:17 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
